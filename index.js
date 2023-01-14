32d8a1938bcf21df430c91191d054c36e76cdf1e
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-13 20:35:02 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'you';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'me';
+}
